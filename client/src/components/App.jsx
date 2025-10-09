ac92c1fa5646ed81cf44c07ec77600403cc22973
Kosyrev143 <artemkosyrev93@gmail.com>
2025-10-09 21:50:42 +0500

    feat: (client) add components

---
+++ client/src/components/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from "./navbar/Navbar";
+import './app.less';
+
+function App() {
+  return (
+    <div className='app'>
+        <Navbar />
+        weklmfkwm
+    </div>
+  );
+}
+
+export default App;
